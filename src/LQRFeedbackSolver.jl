bb04f04cf5ae354a665781196db0d8609e139bfa
Hamzah Khan <2308491+khanh111@users.noreply.github.com>
2022-12-01 13:57:17 -0600

    Make LQR optimal control solver able to accept LTV dynamics and QTV costs.

--- src/LQRFeedbackSolver.jl
+++ src/LQRFeedbackSolver.jl
@@ -3,17 +3,24 @@ using LinearAlgebra
 # Solve a finite horizon, discrete time LQR problem.
 # Returns feedback matrices P[:, :, time].
 
-# Shorthand function for LTI dynamics and costs.
-export solve_lqr_feedback
-function solve_lqr_feedback(dyn::LinearDynamics, costs::QuadraticCost, horizon::Int)
+# Shorthand function for LQ time-invariant dynamics and costs.
+function solve_lqr_feedback(dyn::LinearDynamics, cost::QuadraticCost, horizon::Int)
     dyns = [dyn for _ in 1:horizon]
-    costs = [costs for _ in 1:horizon]
+    costs = [cost for _ in 1:horizon]
     return solve_lqr_feedback(dyns, costs, horizon)
 end
 
-# TODO(hamzah): Add interfaces for cases in which one of the arguments is passed in as a list, but the other is not.
-export solve_lqr_feedback
-function solve_lqr_feedback(dyns::AbstractArray{LinearDynamics}, costs::AbstractArray{QuadraticCost}, horizon::Int)
+function solve_lqr_feedback(dyn::LinearDynamics, costs::AbstractVector{QuadraticCost}, horizon::Int)
+    dyns = [dyn for _ in 1:horizon]
+    return solve_lqr_feedback(dyns, costs, horizon)
+end
+
+function solve_lqr_feedback(dyns::AbstractVector{LinearDynamics}, cost::QuadraticCost, horizon::Int)
+    costs = [cost for _ in 1:horizon]
+    return solve_lqr_feedback(dyns, costs, horizon)
+end
+
+function solve_lqr_feedback(dyns::AbstractVector{LinearDynamics}, costs::AbstractVector{QuadraticCost}, horizon::Int)
 
     # Ensure the number of dynamics and costs are the same as the horizon.
     @assert(ndims(dyns) == 1 && size(dyns, 1) == horizon)
@@ -51,3 +58,32 @@ function solve_lqr_feedback(dyns::AbstractArray{LinearDynamics}, costs::Abstract
 
     return Ps
 end
+
+
+# Solve a finite horizon, discrete time LQR problem by approximating non-LQ dynamics/costs as LQ at each timestep.
+# Returns feedback matrices P[:, :, time].
+
+# A function which accepts non-linear dynamics and non-quadratic costs and solves an LQ approximation at each timestep.
+function solve_approximated_lqr_feedback(dyn::Dynamics,
+                                         cost::Cost,
+                                         horizon::Int,
+                                         t0::Float64,
+                                         x_refs::AbstractArray{Float64},
+                                         u_refs::AbstractArray{Float64})
+    T = horizon
+    N = num_agents(dyn)
+
+    lin_dyns = Array{LinearDynamics}(undef, T)
+    quad_costs = Array{QuadraticCost}(undef, T)
+
+    for tt in 1:T
+        # u_refs_at_tt = [u_refs[ii][tt, :] for ii in 1:N]
+        current_time = t0 + tt
+        lin_dyns[tt] = linearize_dynamics(dyn, current_time, x_refs[tt, :], [u_refs[tt, :]])
+        quad_costs[tt] = quadraticize_costs(cost, current_time, x_refs[tt, :], [u_refs[tt, :]])
+    end
+
+    return solve_lqr_feedback(lin_dyns, quad_costs, T)
+end
+
+export solve_lqr_feedback, solve_approximated_lqr_feedback
