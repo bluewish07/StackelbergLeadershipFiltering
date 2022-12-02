ef53507660b04d67a126da3604dcc5904c7d7324
Hamzah Khan <2308491+khanh111@users.noreply.github.com>
2022-12-01 18:33:34 -0600

    Implement approximated solver for Nash games and LTV functionality for LQ Nash games.

--- src/LQNashFeedbackSolver.jl
+++ src/LQNashFeedbackSolver.jl
@@ -1,10 +1,15 @@
-# A helper function to compute P for all players at time t.
-# TODO: Add the QuadraticCost modifier to this costs_at_t argument.
-function compute_P_at_t(dyn_at_t::LinearDynamics, costs_at_t, Zₜ₊₁)
+# A file containing functions that solve LQ and approximated LQ Nash games for N players.
+# TODO: This doesn't currently support cross-control costs.
 
-    num_players = size(costs_at_t)[1]
+# A helper function to compute P for all players at time t.
+# TODO: The solver as currently written doesn't handle different sized control vectors. Fix this (p. 33 of class notes).
+function compute_P_at_t(dyn_at_t::LinearDynamics, costs_at_t::AbstractVector{QuadraticCost}, Zₜ₊₁)
+
+    num_players = num_agents(dyn_at_t)
     num_states = xdim(dyn_at_t)
     A = dyn_at_t.A
+
+    # TODO: This line breaks very easily under weirdly-dimensioned systems. Change the code here.
     lhs_rows = Array{Float64}(undef, 0, num_states ÷ num_players)
 
     for player_idx in 1:num_players
@@ -33,54 +38,70 @@ function compute_P_at_t(dyn_at_t::LinearDynamics, costs_at_t, Zₜ₊₁)
 end
 
 # Solve a finite horizon, discrete time LQ game to feedback Nash equilibrium.
-# Returns feedback matrices P[player][:, :, time]
-export solve_lq_nash_feedback
+# Returns feedback matrices P[player][:, :, time] and state costs matrices Z[player][:, :, time]
+# The number of players, states, and control sizes are assumed to be constant over time.
 function solve_lq_nash_feedback(
-    dyn::LinearDynamics, costs::AbstractArray{QuadraticCost}, horizon::Int)
+    dyns::AbstractVector{LinearDynamics}, all_costs::AbstractVector{<:AbstractVector{QuadraticCost}}, horizon::Int)
 
-    num_players = size(costs)[1]
+    # Ensure the number of dynamics and costs are the same as the horizon.
+    @assert !isempty(dyns) && size(dyns, 1) == horizon
+    @assert !isempty(all_costs) && size(all_costs, 1) == horizon
 
-    # 1. Start at t=T, setting Z^i_t = Q^i_t.
-    t = horizon
-    Zₜ₊₁ = [costs[i].Q for i in 1:num_players]
-    Zₜ = [zeros(size(Zₜ₊₁[i])) for i in 1:num_players]
+    # Ensure that the number of controls matches number of players at each horizon.
+    for tt in 1:horizon
+        @assert(size(all_costs[tt], 1) == num_agents(dyns[tt]))
+    end
 
-    num_states = xdim(dyn)
-    all_Zs = [zeros(num_states, num_states, horizon) for i in 1:num_players]
-    all_Ps = [zeros(udim(dyn, i), num_states, horizon) for i in 1:num_players]
+    # The number of players, states, and control sizes are assumed to be constant over time.
+    num_players = num_agents(dyns[1])
+    num_states = xdim(dyns[1])
 
-    all_Zs[1][:, :, horizon] = costs[1].Q
-    all_Zs[2][:, :, horizon] = costs[2].Q
+    # Initialize the feedbacks gains and state costs.
+    all_Zs = [zeros(num_states, num_states, horizon) for _ in 1:num_players]
+    all_Ps = [zeros(udim(dyns[1], ii), num_states, horizon) for ii in 1:num_players]
 
-    for t = horizon-1:-1:1
+    # 1. Start at the final timestep (t=T), setting Z^i_T = Q^i_T.
+    Zₜ₊₁ = [all_costs[horizon][ii].Q for ii in 1:num_players]
+    Zₜ = [zeros(size(Zₜ₊₁[ii])) for ii in 1:num_players]
+
+    for ii in 1:num_players
+        all_Zs[ii][:, :, horizon] = all_costs[horizon][ii].Q
+    end
+
+    for tt = horizon-1:-1:1
+
+        # Get the dynamics and costs at the current time.
+        dyn = dyns[tt]
+        costs = all_costs[tt]
 
         # 2. Compute P^{i*}_t and Z^i_t for all players at time t and store them.
         Ps = compute_P_at_t(dyn, costs, Zₜ₊₁)
-        for i in 1:num_players
-            num_inputs = udim(dyn, i)
-            index_range = (i-1) * num_inputs + 1 : i * num_inputs
+        for ii in 1:num_players
+            num_inputs = udim(dyn, ii)
+            index_range = (ii-1) * num_inputs + 1 : ii * num_inputs
 
-            all_Ps[i][:, :, t] = reshape(Ps[index_range, :], (num_inputs, xdim(dyn), 1))
+            all_Ps[ii][:, :, tt] = reshape(Ps[index_range, :], (num_inputs, xdim(dyn), 1))
         end
 
-        for i in 1:num_players
+        for ii in 1:num_players
             # Extract other values.
-            Qₜ = [costs[i].Q for i in 1:num_players]
-            Pⁱₜ = all_Ps[i][:, :, t]
+            Qₜ = [costs[ii].Q for i in 1:num_players]
+            Pⁱₜ = all_Ps[ii][:, :, tt]
 
             # Compute Z terms. There are no nonzero off diagonal Rij terms, so we just need to compute the terms with Rii.
-            summation_1_terms = [Pⁱₜ' * costs[i].Rs[i] * Pⁱₜ]
+            summation_1_terms = [Pⁱₜ' * costs[ii].Rs[ii] * Pⁱₜ]
             summation_1 = sum(summation_1_terms)
 
-            summation_2_terms = [dyn.Bs[j] * all_Ps[j][:, :, t] for j in 1:num_players]
+            summation_2_terms = [dyn.Bs[jj] * all_Ps[jj][:, :, tt] for jj in 1:num_players]
             summation_2 = dyn.A - sum(summation_2_terms)
 
-            Zₜ[i] = Qₜ[i] + summation_1 + summation_2' * Zₜ₊₁[i] * summation_2
+            Zₜ[ii] = Qₜ[ii] + summation_1 + summation_2' * Zₜ₊₁[ii] * summation_2
         end
 
         # Update Z_{t+1}
-        all_Zs[1][:, :, t] = Zₜ[1]
-        all_Zs[2][:, :, t] = Zₜ[2]
+        for ii in 1:num_players
+            all_Zs[ii][:, :, tt] = Zₜ[ii]
+        end
         Zₜ₊₁ = Zₜ
 
         # 3. Go to (2) until t=1.
@@ -88,3 +109,52 @@ function solve_lq_nash_feedback(
 
     return all_Ps, all_Zs
 end
+
+# Shorthand function for LQ time-invariant dynamics and costs.
+function solve_lq_nash_feedback(dyn::LinearDynamics, costs::AbstractVector{QuadraticCost}, horizon::Int)
+    dyns = [dyn for _ in 1:horizon]
+    all_costs = [costs for _ in 1:horizon]
+    return solve_lq_nash_feedback(dyns, all_costs, horizon)
+end
+
+function solve_lq_nash_feedback(dyn::LinearDynamics, all_costs::AbstractVector{<:AbstractVector{QuadraticCost}}, horizon::Int)
+    dyns = [dyn for _ in 1:horizon]
+    return solve_lq_nash_feedback(dyns, all_costs, horizon)
+end
+
+function solve_lq_nash_feedback(dyns::AbstractVector{LinearDynamics}, costs::AbstractVector{QuadraticCost}, horizon::Int)
+    all_costs = [costs for _ in 1:horizon]
+    return solve_lq_nash_feedback(dyns, all_costs, horizon)
+end
+
+
+# A function which accepts non-linear dynamics and non-quadratic costs and solves an LQ approximation at each timestep.
+function solve_approximated_lq_nash_feedback(dyn::Dynamics,
+                                             costs::AbstractVector{<:Cost},
+                                             horizon::Int,
+                                             t0::Float64,
+                                             x_refs::AbstractArray{Float64},
+                                             u_refs::AbstractVector{<:AbstractArray{Float64}})
+    T = horizon
+    N = num_agents(dyn)
+
+    lin_dyns = Array{LinearDynamics}(undef, T)
+    all_quad_costs = Vector{Vector{QuadraticCost}}(undef, T)
+
+    for tt in 1:T
+        quad_costs = Vector{QuadraticCost}(undef, N)
+        u_refs_at_tt = [u_refs[ii][:, tt] for ii in 1:N]
+        current_time = t0 + tt
+
+        # Linearize and quadraticize the dynamics/costs.
+        lin_dyns[tt] = linearize_dynamics(dyn, current_time, x_refs[:, tt], u_refs_at_tt)
+        for ii in 1:N
+            quad_costs[ii] = quadraticize_costs(costs[ii], current_time, x_refs[:, tt], u_refs_at_tt)
+        end
+        all_quad_costs[tt] = quad_costs
+    end
+
+    return solve_lq_nash_feedback(lin_dyns, all_quad_costs, T)
+end
+
+export solve_lq_nash_feedback, solve_approximated_lq_nash_feedback
--- test/TestApproximatedLQSolvers.jl
+++ test/TestApproximatedLQSolvers.jl
@@ -14,13 +14,15 @@ seed!(0)
     stackelberg_leader_idx = 1
 
     t0 = 0.0
-    x₁ = [1., 0, 1, 0]
+    x₁ = [1.; 0; 1.; 0]
+    x₁ = vcat(x₁, x₁)
+    num_states = size(x₁, 1)
     horizon = 10
 
     # Ensure that for an LQ optimal control problem, both the LQ optimal control solution and approximate LQ Nash
     # optimal control with the solution to the LQ optimal control problem as reference, are identical!
     @testset "CheckLQOptCtrlAndApproximatedLQOptCtrlGiveSameOutputForLQGame" begin
-        sys_info = SystemInfo(1, 4, [2])
+        sys_info = SystemInfo(1, num_states, [2])
         dyn = generate_random_linear_dynamics(sys_info)
         costs = generate_random_quadratic_costs(sys_info; include_cross_costs=true)
 
@@ -31,4 +33,25 @@ seed!(0)
         @test Ps == P̃s
         @test Zs == Z̃s
     end
+
+
+    # Ensure that for an LQ game, both the LQ Nash solution and approximate LQ Nash solution with the solution to the
+    # LQ Nash game as reference, are identical!
+    @testset "CheckLQNashAndApproximatedLQNashGiveSameOutputForLQGame" begin
+        num_players = 2
+        num_ctrls = [2, 2]
+        sys_info = SystemInfo(num_players, num_states, num_ctrls)
+
+        # Generate the game.
+        dyn = generate_random_linear_dynamics(sys_info)
+        costs = generate_random_quadratic_costs(sys_info; include_cross_costs=true)
+
+        Ps, Zs = solve_lq_nash_feedback(dyn, costs, horizon)
+        xs, us = unroll_feedback(dyn, Ps, x₁)
+        P̃s, Z̃s = solve_approximated_lq_nash_feedback(dyn, costs, horizon, t0, xs, us)
+
+        @test Ps == P̃s
+        @test Zs == Z̃s
+    end
+
 end
