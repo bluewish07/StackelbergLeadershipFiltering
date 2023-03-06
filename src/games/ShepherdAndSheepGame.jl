cff8abc9cff94296587b47b9ccef5419038e7e0d
Hamzah Khan <2308491+khanh111@users.noreply.github.com>
2023-03-05 21:13:13 -0600

    Add shepherd and sheep game, was somehow not added.

---
+++ src/games/ShepherdAndSheepGame.jl
@@ -0,0 +1,63 @@
+# This file defines the dynamics and cost functions for the Shepherd and Sheep example.
+
+# Two player dynamics
+# Dynamics (Euler-discretized double integrator equations with Δt = 0.1s).
+# State for each player is layed out as [x, ẋ, y, ẏ].
+
+using LinearAlgebra
+
+num_players = 2
+num_states = 8
+num_ctrls = [2, 2]
+
+shepherd_Ã(dt) = [1 dt  0  0;
+                  0  1  0  0;
+                  0  0  1 dt;
+                  0  0  0  1]
+shepherd_A(dt) = vcat(hcat(shepherd_Ã(dt), zeros(4, 4)),
+                      hcat(zeros(4, 4), shepherd_Ã(dt)))
+
+B₁(dt) = vcat([0   0;
+               dt  0;
+               0   0;
+               0   dt],
+               zeros(4, 2))
+B₂(dt) = vcat(zeros(4, 2),
+              [0   0;
+               dt  0;
+               0   0;
+               0   dt])
+
+ShepherdAndSheepDynamics(dt) = LinearDynamics(shepherd_A(dt), [B₁(dt), B₂(dt)])
+
+# Costs reflecting the preferences above.
+Q₁ = zeros(8, 8)
+Q₁[5, 5] = 1.0
+Q₁[7, 7] = 1.0
+c₁ = QuadraticCost(Q₁)
+add_control_cost!(c₁, 1, 1 * diagm([1, 1]))
+add_control_cost!(c₁, 2, zeros(2, 2))
+
+Q₂ = zeros(8, 8)
+Q₂[1, 1] = 1.0
+Q₂[5, 5] = 1.0
+Q₂[1, 5] = -1.0
+Q₂[5, 1] = -1.0
+Q₂[3, 3] = 1.0
+Q₂[7, 7] = 1.0
+Q₂[3, 7] = -1.0
+Q₂[7, 3] = -1.0
+c₂ = QuadraticCost(Q₂)
+add_control_cost!(c₂, 2, 1 * diagm([1, 1]))
+add_control_cost!(c₂, 1, zeros(2, 2))
+
+dummy_time_range = (-1.0, -1.0)
+dummy_x = zeros(num_states)
+dummy_us = [zeros(num_ctrls[ii]) for ii in 1:num_players]
+costs = [quadraticize_costs(c₁, dummy_time_range, dummy_x, dummy_us),
+         quadraticize_costs(c₂, dummy_time_range, dummy_x, dummy_us)]
+
+# Gets a vector of costs, one per player.
+ShepherdAndSheepCosts() = costs
+
+export ShepherdAndSheepDynamics, ShepherdAndSheepCosts
